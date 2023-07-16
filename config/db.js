32ba330b29e64be986074e7c56dc898c15264b17
arabmakloem <37822214+dewabagas@users.noreply.github.com>
2023-07-16 17:07:48 +0700

    back to default pool

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 const Pool = require('pg').Pool
 require('dotenv').config();
 
-// const pool = new Pool ({
-//     user : process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     database: process.env.DB_DATABASE,
-//     password: process.env.DB_PASSWORD
-// })
-
-const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+const pool = new Pool ({
+    user : process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD
 })
+
+// const pool = new Pool({
+//     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+// })
 
 module.exports = pool
 
