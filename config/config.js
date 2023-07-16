1ecb033231a4ab4b4b4fddfd1a165fe0dd83b872
arabmakloem <37822214+dewabagas@users.noreply.github.com>
2023-07-16 18:01:16 +0700

    comment use_env_variable

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
-        use_env_variable: process.env.DB_URL,
+        // use_env_variable: process.env.DB_URL,
         port: process.env.DB_PORT,
         host: process.env.DB_HOST,
         dialect: 'postgres',
