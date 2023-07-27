37d12dac818aec0c811f08dcb788a2076f8c1abc
dewabagas <dewabagasql@gmail.com>
2023-07-27 18:50:48 +0700

    add podcast to seeders

--- seeders/20230701154327-quiz_attempts.js
+++ seeders/20230701154327-quiz_attempts.js
@@ -2,27 +2,29 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('quiz_attempts', [{
-      user_id: 1,
-      quiz_id: 1,
-      start_time: new Date(),
-      end_time: new Date(),
-      correct_answers: 15,
-      incorrect_answers: 5,
-      score: 75,
-      created_at: new Date(),
-      updated_at: new Date()
-    },{
-      user_id: 2,
-      quiz_id: 2,
-      start_time: new Date(),
-      end_time: new Date(),
-      correct_answers: 10,
-      incorrect_answers: 10,
-      score: 50,
-      created_at: new Date(),
-      updated_at: new Date()
-    }], {});
+    return queryInterface.bulkInsert('quiz_attempts', [
+    //   {
+    //   user_id: 1,
+    //   quiz_id: 1,
+    //   start_time: new Date(),
+    //   end_time: new Date(),
+    //   correct_answers: 15,
+    //   incorrect_answers: 5,
+    //   score: 75,
+    //   created_at: new Date(),
+    //   updated_at: new Date()
+    // },{
+    //   user_id: 2,
+    //   quiz_id: 2,
+    //   start_time: new Date(),
+    //   end_time: new Date(),
+    //   correct_answers: 10,
+    //   incorrect_answers: 10,
+    //   score: 50,
+    //   created_at: new Date(),
+    //   updated_at: new Date()
+    // }
+  ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
