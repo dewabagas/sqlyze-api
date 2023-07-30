590643f2cc7a3bf7132e06216c15049a97bdbfaa
dewabagas <dewabagasql@gmail.com>
2023-07-30 21:15:16 +0700

    add update profile function

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { User, Profile } = require('../models');
 require('dotenv').config();
+const upload = require('../middlewares/multer.middleware');
 
 exports.getProfile = async (req, res) => {
   try {
@@ -25,6 +26,46 @@ exports.getProfile = async (req, res) => {
     res.status(500).send({
       status: "FAILED",
       message: "Profile fetching failed",
+      error: error.message,
+    });
+  }
+};
+
+exports.updateProfile = async (req, res) => {
+  const userId = req.id;
+  const { full_name, email, nis, msisdn, birthdate, gender } = req.body;
+  let profile_image_url = req.file ? req.file.path : null;
+
+  try {
+    const user = await User.findOne({ where: { id: userId } });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const updatedProfileData = { full_name, email, nis, msisdn, birthdate, gender };
+    if (profile_image_url) updatedProfileData.profile_image_url = profile_image_url;
+
+    const profile = await Profile.update(updatedProfileData,
+      { where: { user_id: user.id } },
+      { returning: true } // This will return the updated profile
+    );
+
+    if (!profile) {
+      return res.status(400).json({ message: 'Failed to update profile' });
+    }
+
+    return res.status(200).json({
+      status: 'SUCCESS',
+      code: 200,
+      message: 'Profile updated successfully',
+      result: profile,
+    });
+
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({
+      status: 'FAILED',
+      message: 'Failed to update profile',
       error: error.message,
     });
   }
--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,11 @@ const profileController = require('../controllers/profile.controller')
 const user = require('../middlewares/user');
 const middleware = require('../middlewares/auth');
 const { verify } = require('../middlewares/auth');
+const upload = require('../middlewares/multer.middleware');
 
 router.post('/register', user.validateUserRegister, controller.register);
 router.post('/login', user.validateUserLogin, controller.login);
 router.get('/profile', verify, profileController.getProfile);
-// router.put('/:userId', middleware.verify, middleware.authorization, user.validateUserUpdate,  controller.editUser);
-// router.delete('/:userId', middleware.verify, middleware.authorization, controller.deleteUser);
+router.put('/profile/edit', verify, upload.single('profile_image'), profileController.updateProfile);
 
 module.exports = router;
